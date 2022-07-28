30f70b6c8d72701fd6afb3eaa10c5092227c5230
Brexh Maixh <oriemabrender00@gmail.com>
2022-07-28 21:08:43 +0300

    Classpracticecmad

---
+++ Todo-App/todos-app.js
@@ -0,0 +1,6 @@
+console.log("Javascript");
+
+//add new element
+const newparagraph = document.createElement("p");
+newparagraph.textContent = "Brender dancing";
+document.querySelector("body").appendChild(newparagraph);
